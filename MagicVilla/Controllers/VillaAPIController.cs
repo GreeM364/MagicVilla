78c6cca2e2c71e93bfb9dbad37353521615eed67
Denis <denisrubel40@gmail.com>
2023-03-28 15:09:05 +0300

    Added first endpoints for VillaAPIController

---
+++ MagicVilla/Controllers/VillaAPIController.cs
@@ -0,0 +1,23 @@
+﻿using MagicVilla.Data;
+using MagicVilla.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas() 
+        {
+            return VillaStore.villaList;            
+        }
+
+        [HttpGet("{id}")]
+        public VillaDTO? GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
