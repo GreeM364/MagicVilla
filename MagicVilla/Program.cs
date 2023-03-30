0635dbef318b5751c306e31349324eb0c840c71c
Denis <denisrubel40@gmail.com>
2023-03-30 13:31:37 +0300

    Added logging for VillaAPIController

--- MagicVilla/Program.cs
+++ MagicVilla/Program.cs
@@ -1,11 +1,17 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().
+    WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
