b294c9e17657afad5c9efd26f37be8504e3b7b30
Denis <denisrubel40@gmail.com>
2023-04-06 01:02:29 +0300

    Added pagination in API

--- MagicVilla/Controllers/v1/VillaAPIController.cs
+++ MagicVilla/Controllers/v1/VillaAPIController.cs
@@ -5,6 +5,7 @@ using MagicVilla.Repository.IRepository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 using System.Net;
 
 namespace MagicVilla.Controllers.v1
@@ -32,7 +33,7 @@ namespace MagicVilla.Controllers.v1
         [ResponseCache(CacheProfileName = "Default30")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")] int? occupancy,
-                                                               [FromQuery] string? search)
+                                                     [FromQuery] string? search, int pageSize = 4, int pageNumber = 1)
         {
             try
             {
@@ -40,16 +41,20 @@ namespace MagicVilla.Controllers.v1
 
                 if (occupancy > 0)
                 {
-                    villas = await _villaRepository.GetAllAsync(u => u.Occupancy == occupancy);
+                    villas = await _villaRepository.GetAllAsync(u => u.Occupancy == occupancy, pageSize: pageSize,
+                                                                pageNumber: pageNumber);
                 }
                 else
                 {
-                    villas = await _villaRepository.GetAllAsync();
+                    villas = await _villaRepository.GetAllAsync(pageSize: pageSize, pageNumber: pageNumber);
                 }
                 if (!string.IsNullOrEmpty(search))
                 {
                     villas = villas.Where(u => u.Name.ToLower().Contains(search) || u.Amenity.ToLower().Contains(search));
                 }
+
+                Pagination pagination = new() { PageNumber = pageNumber, PageSize = pageSize };
+                Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(pagination));
 
                 _response.Result = _mapper.Map<List<VillaDTO>>(villas);
                 _response.StatusCode = HttpStatusCode.OK;
